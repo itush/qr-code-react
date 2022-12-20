d3e7abec34605d7da5acfcbd573698e26c3a861d
itush <itusharbiswas@gmail.com>
2022-12-20 10:41:50 +0530

    footer top margin modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            <div className='text-xs text-center mt-[25%]'>
+            <div className='text-xs text-center mt-[20%]'>
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className='text-blue-800'>Frontend Mentor</a>.
                 Coded by <a href="#" className='text-blue-800'>Your Name Here</a>.
             </div>
